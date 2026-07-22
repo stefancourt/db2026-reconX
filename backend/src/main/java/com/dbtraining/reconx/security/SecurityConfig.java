c2d7d2ac7ffae027c0d61f0f2a4279f7fd18fc67
MirceaOvidiu <mircea.ovidiu.03@gmail.com>
2026-07-22 16:27:46 +0300

    fix/compose runtime achieved

--- backend/src/main/java/com/dbtraining/reconx/security/SecurityConfig.java
+++ backend/src/main/java/com/dbtraining/reconx/security/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.dbtraining.reconx.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -56,6 +58,11 @@ import org.springframework.security.web.SecurityFilterChain;
  */
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
