5ec3dbcca71ca86da00ac34355579e3066c71356
Sean Mata <matasean@yahoo.com>
2026-07-27 12:31:58 +0100

    Indent error

--- backend/src/main/java/com/dbtraining/reconx/model/BondTrade.java
+++ backend/src/main/java/com/dbtraining/reconx/model/BondTrade.java
@@ -68,8 +68,8 @@ public final class BondTrade implements TradeType {
     }
 
     @Override public String toString() {
-        // TODO(TICKET-ADV030): "BondTrade[ref=..., isin=..., face=... CCY, coupon=..., maturity=..., side=...]"
-        
+        //NOTE: counterpartId is ommitted to remove PII from logs.
+        return "BondTrade[ref=%s, isin=%s, face=%s %s, coupon=%s, maturity=%s, side=%s]".formatted(tradeRef, isin, faceValue, currency.getCurrencyCode(), couponRate, maturityDate, side);
     }
 
     public static final class Builder {
